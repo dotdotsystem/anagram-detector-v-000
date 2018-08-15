38dd4b7f81b80bd5e2a9ee0c14ea52de25edd389
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-15 04:10:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,15 +7,9 @@ class Anagram
   end
 
   def match (array)
-     anagrams = []
 
-     array.each do |x|
-       if x.split("").sort == @word.split("").sort
-           anagrams << x
-       else
-         anagrams
-       end
-     end
-     anagrams
-   end
+     array.select do |w|
+        x.split("").sort == @word.split("").sort
+
+ end
  end
